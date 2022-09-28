1e383a591741382458f15250181884a45cafdbe8
Sudhir Haridas Kumbhar <105354469+kumbharsudhir@users.noreply.github.com>
2022-09-28 14:20:39 +0530

    Initial commit

---
+++ firstpro.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class firstpro {
+	public static void main(String args[])
+	{	  
+		  System.out.println("Hello Developers"); 
+		System.out.println("Hello My Dear Friends");  
+	}
+	}
+}
